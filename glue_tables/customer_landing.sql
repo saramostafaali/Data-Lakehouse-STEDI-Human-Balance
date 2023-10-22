97be48fd2a70180ff3126a034ea663d9a4028f6a
Sara Mostafa <75923531+saramostafaali@users.noreply.github.com>
2023-10-23 02:25:51 +0300

    Create customer_landing.sql

---
+++ glue_tables/customer_landing.sql
@@ -0,0 +1,22 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi2`.`customer_landing` (
+  `customerName` string,
+  `email` string,
+  `phone` string,
+  `birthDay` string,
+  `serialNumber` string,
+  `registrationDate` bigint,
+  `lastUpdateDate` bigint,
+  `shareWithResearchAsOfDate` float,
+  `shareWithPublicAsOfDate` float,
+  `shareWithFriendsAsOfDate` float
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://stedi-hb-lakehouse/src/customer/landing/'
+TBLPROPERTIES ('classification' = 'json');
